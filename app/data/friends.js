f7d960275012f3cd3e6c2b50f419066e4063793a
Brendan McBryan <brendan.mcbryan@gmail.com>
2019-08-11 16:01:05 -0400

    added a nice array for possible friends

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,16 +1,60 @@
 const friendTable = [
   {
-    name: "Lucy",
+    name: "Karl",
     photo:
-      "https://amp.businessinsider.com/images/53de9d606bb3f70125e5f783-750-456.jpg",
-    scores: [3, 5, 4, 1, 3, 2, 5, 1, 3, 2]
+      "http://www3.pictures.gi.zimbio.com/Karl+Marx+Anne+Frank+Wax+Figure+Madame+Tussauds+__4SUkOxl5jl.jpg",
+    scores: [3, 5, 4, 1, 3, 2, 5, 1, 3, 5]
   },
   {
-    name: "Steve",
+    name: "Peter",
     photo:
-      "https://vignette.wikia.nocookie.net/familymatters/images/8/8f/Steve_Urkel_posing.jpg/revision/latest?cb=20170308034914",
-    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/Peter_Kropotkin_circa_1900.jpg/220px-Peter_Kropotkin_circa_1900.jpg",
+    scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 5]
+  },
+  {
+    name: "Rosa",
+    photo:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/5/52/Rosa_Luxemburg.jpg/220px-Rosa_Luxemburg.jpg",
+    scores: [2, 3, 4, 1, 2, 4, 5, 3, 1, 5]
+  },
+  {
+    name: "Leon",
+    photo:
+      "http://img.timeinc.net/time/photoessays/2010/top10_assassinations/leon_trotsky.jpg",
+    scores: [5, 3, 5, 1, 3, 4, 1, 2, 1, 5]
+  },
+  {
+    name: "Thomas",
+    photo: "https://www.newframe.com/wp-content/uploads/2019/03/image-71.png",
+    scores: [2, 3, 4, 3, 5, 1, 2, 1, 2, 5]
+  },
+  {
+    name: "Vladimir",
+    photo: "https://www.onthisday.com/images/people/vladimir-lenin-medium.jpg",
+    scores: [1, 2, 5, 3, 3, 1, 5, 4, 3, 5]
+  },
+  {
+    name: "Martin",
+    photo:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Martin_Luther_King%2C_Jr..jpg/440px-Martin_Luther_King%2C_Jr..jpg",
+    scores: [5, 2, 4, 2, 3, 4, 3, 1, 2, 3]
+  },
+  {
+    name: "Freddy",
+    photo:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/Friedrich_Engels_portrait_%28cropped%29.jpg/220px-Friedrich_Engels_portrait_%28cropped%29.jpg",
+    scores: [2, 4, 2, 4, 3, 2, 4, 1, 5, 5]
+  },
+  {
+    name: "Helen",
+    photo: "https://cdn.britannica.com/s:300x300/08/19608-004-D95EA290.jpg",
+    scores: [2, 3, 4, 2, 1, 4, 3, 5, 5, 5]
+  },
+  {
+    name: "Clara",
+    photo: "https://upload.wikimedia.org/wikipedia/commons/1/10/C_Zetkin_1.jpg",
+    scores: [1, 3, 2, 1, 4, 5, 5, 5, 5, 5]
   }
 ];
-// Note how we export the array. This makes it accessible to other files using require.
+
 module.exports = friendTable;
