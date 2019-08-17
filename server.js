5aa819a5f1b9c4c237b34bc050dd23bc97aed47e
Brendan McBryan <brendan.mcbryan@gmail.com>
2019-08-17 17:18:55 -0400

    final changes

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require("path");
 
 // Sets up the Express App
 var app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(
